bcd5bf64bd3d0261798cf349da477d1bddf12cdf
Przemysław Papla <przemekpapla@gmail.com>
2020-08-18 12:27:17 +0200

    remove cmake/make from pq

--- contrib/libpqxx-6.4.7/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/przemek/Downloads/libpqxx-6.4.7/test
-# Build directory: /home/przemek/Downloads/libpqxx-6.4.7/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "/home/przemek/Downloads/libpqxx-6.4.7/build/test/runner")
-subdirs("unit")
--- contrib/libpqxx-6.4.7/test/unit/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/przemek/Downloads/libpqxx-6.4.7/test/unit
-# Build directory: /home/przemek/Downloads/libpqxx-6.4.7/build/test/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_runner "/home/przemek/Downloads/libpqxx-6.4.7/build/test/unit/unit_runner")
